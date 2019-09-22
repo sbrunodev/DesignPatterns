f800cca3301ed1d52035d1192b34c8c8a8779f85
Bruno <brunosilva.dev@outlook.com>
2019-09-22 09:38:21 -0300

    Strategy

---
+++ Comportamental/Strategy/Strategy/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList studentRecords = new SortedList();
+
+            studentRecords.Add("Rodrigo");
+            studentRecords.Add("Maria");
+            studentRecords.Add("João");
+            studentRecords.Add("Carlos");
+            studentRecords.Add("Ana");
+
+            studentRecords.SetSortStategy(new QuickSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStategy(new ShellSort());
+            studentRecords.Sort();
+
+            studentRecords.SetSortStategy(new MergeSort());
+            studentRecords.Sort();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Comportamental/Strategy/Strategy/SortedList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy
+{
+    public class SortedList
+    {
+        private List<string> _list = new List<string>();
+        private SortStrategy _sortstrategy;
+
+        public void SetSortStategy(SortStrategy sortStrategy)
+        {
+            this._sortstrategy = sortStrategy;
+        }
+
+        public void Add(string name)
+        {
+            _list.Add(name);
+        }
+
+        public void Sort()
+        {
+            _sortstrategy.Sort(_list);
+
+            foreach(string name in _list)
+            {
+                Console.WriteLine("   " + name);
+            }
+
+            Console.WriteLine();
+        }
+
+
+    }
+}
