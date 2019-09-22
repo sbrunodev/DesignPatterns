bcea575150b153987fe5b8ac6f239f3710fa7041
Bruno <brunosilva.dev@outlook.com>
2019-09-22 08:38:46 -0300

    Iterator

---
+++ Comportamental/Iterator/Iterator/ConcreteIterator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class ConcreteIterator : Iterator
+    {
+        private ConcreteAggregate _aggregate;
+        private int _current = 0;
+
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this._aggregate = aggregate;
+        }
+
+        public override object CurrentItem()
+        {
+            return this._aggregate[_current];
+        }
+
+        public override object First()
+        {
+            return this._aggregate[0];
+        }
+
+        public override bool IsDone()
+        {
+            return _current >= _aggregate.Count;
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            if (_current < _aggregate.Count - 1)
+                ret = _aggregate[++_current];
+
+            return ret;
+        }
+    }
+}
---
+++ Comportamental/Iterator/Iterator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConcreteAggregate a = new ConcreteAggregate();
+            a[0] = "Item A";
+            a[1] = "Item B";
+            a[2] = "Item C";
+            a[3] = "Item D";
+
+            Iterator i = a.createIterator();
+
+            Console.WriteLine("Interagindo com a comlegação");
+
+            object item = i.First();
+
+            while(item != null)
+            {
+                Console.WriteLine(item);
+                item = i.Next();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
