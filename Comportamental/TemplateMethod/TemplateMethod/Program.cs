a556c7db38a1c912d9ce78fea73d9de5eef67acf
Bruno <brunosilva.dev@outlook.com>
2019-09-22 10:01:11 -0300

    TemplateMethod

---
+++ Comportamental/TemplateMethod/TemplateMethod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplateMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbstractClass abstrata_a = new ConcreteClassA();
+            abstrata_a.TemplateMethod();
+
+
+            AbstractClass abstrata_b = new ConcreteClassB();
+            abstrata_b.TemplateMethod();
+
+            Console.ReadKey();
+        }
+    }
+}
